6926e9d5a9fb2cc83970e093b0a9cc434f6d348d
rishiptyltd <99692690+rishiptyltd@users.noreply.github.com>
2022-07-15 02:27:13 +1000

    Expand README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# exam-timer
- Public domain exam timer built with React and designed for projectors.
+# ⌛ Exam Timer
+> A generic, public domain exam timer built with React and MUI. Designed for use on projectors, smartboards and other extremely large displays.
+
+## User Instructions
+You can find the website [here](). In the future, a desktop Electron app might be provided.
+
+## Developer Instructions
+Clone the repository to any directory, and ensure Node and NPM are installed on your machine before continuing. Navigate to the repository folder on your machine and install the required packages with the command below.
+```js
+npm install
+```
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Fab, Dialog } from '@mui/material';
+import { Container, Fab, Dialog, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/AddOutlined';
 import InfoIcon from '@mui/icons-material/InfoOutlined';
 import NewTimer from './components/NewTimer'
@@ -18,9 +18,13 @@ export default function Home()
         setOpen(false);
     };
 
+    let date = new Date();
 
     return (
         <Container>
+            <Typography variant='overline'>
+                2:25 am <br /> 15/07/2022
+            </Typography>
             <div>
                 <Fab color="warning" aria-label="add" variant='extended' onClick={handleClickOpen}>
                     <InfoIcon sx={{ mr: 0.5 }} fontSize='small' />
