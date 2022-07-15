3032db0538bfad88e8170bee1fc8b22c029605bb
rishi <99692690+rishiptyltd@users.noreply.github.com>
2022-07-15 12:47:54 +1000

    Add Basic Components without Functionality

--- src/pages/components/NewTimer.jsx
+++ src/pages/components/NewTimer.jsx
@@ -4,28 +4,6 @@ import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, T
 export default function NewTimer()
 {
     return (
-        <div>
-            <DialogTitle>Add New Timer</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                    To subscribe to this website, please enter your email address here. We
-                    will send updates occasionally.
-                </DialogContentText>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="name"
-                    label="Email Address"
-                    type="email"
-                    fullWidth
-                    variant="standard"
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleClose}>Finish</Button>
-            </DialogActions>
-
-        </div>
+        <div></div>
     )
 }
